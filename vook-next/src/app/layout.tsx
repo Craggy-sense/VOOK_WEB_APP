b1bf8fb60f888c43a235ae42a87f9b45c21cda6a
Craggy-sense <memusirobi@gmail.com>
2026-06-14 22:09:03 +0300

    Fix Vercel build: Duplicate React instances and layout head warnings

--- vook-next/src/app/layout.tsx
+++ vook-next/src/app/layout.tsx
@@ -48,10 +48,8 @@ export default function RootLayout({
 
   return (
     <html lang="en">
-      <head>
-        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
-      </head>
       <body style={{ '--primary-green': settings.primary_color } as React.CSSProperties}>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.4.0/css/all.min.css" />
         <Header settings={settings} />
         <main>{children}</main>
         <Footer settings={settings} />
