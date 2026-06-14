386137fadf8aa156592cf5f8a7524f9e46ceb332
Craggy-sense <memusirobi@gmail.com>
2026-06-14 11:26:18 +0300

    Launch real homepage, remove Coming Soon

--- vook-next/src/app/page.original.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import client from "../../tina/__generated__/client";
-import HomePage from "./home-page";
-
-export default async function Home() {
-  const result = await client.queries.pages({ relativePath: "home.md" });
-  return <HomePage {...result} />;
-}
